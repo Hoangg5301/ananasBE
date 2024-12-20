0a4e60fe421a82b3285eaa9e07afc51016a6baea
HoangNguyen <hoangnguyen5301@gmail.com>
2024-12-20 07:47:04 +0700

    config Seucurity

--- src/main/java/com/example/ananasstore/configuration/SercurityConfig.java
+++ src/main/java/com/example/ananasstore/configuration/SercurityConfig.java
@@ -1,21 +1,45 @@
 package com.example.ananasstore.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.AbstractConfiguredSecurityBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.crypto.spec.SecretKeySpec;
 
 @Configuration
 @EnableWebSecurity
 public class SercurityConfig {
+
+    @Value("${jwt.key}")
+    private String signingKey;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.authorizeRequests(requests -> requests.anyRequest().authenticated());
-                .anyRequest().authenticated();
+        httpSecurity.csrf(httpSecurityCsrfConfigurer -> {httpSecurityCsrfConfigurer.disable();})
+                .authorizeHttpRequests(request ->
+                request.requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
+                        .anyRequest().authenticated());
 
-        httpSecurity.csrf(httpSecurityCsrfConfigurer -> {httpSecurityCsrfConfigurer.disable();});
+        httpSecurity.oauth2ResourceServer(oauth2ResourceServerConfigurer -> {
+            oauth2ResourceServerConfigurer.jwt(jwtConfigurer -> jwtConfigurer.decoder(jwtDecoder()));
+        });
         return httpSecurity.build();
+    }
+
+    @Bean
+    JwtDecoder jwtDecoder() {
+        SecretKeySpec secretKeySpec = new SecretKeySpec(signingKey.getBytes(), "HS512");
+        return NimbusJwtDecoder
+                .withSecretKey(secretKeySpec)
+                .macAlgorithm(MacAlgorithm.HS512)
+                .build();
     }
 }
